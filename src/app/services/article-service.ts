40ddca8d132731cc974e281fb0f08e672e90b118
luca <luca.argentino@outlook.com>
2022-04-20 15:39:36 +0200

    Inject ArticleRepositoryService

--- src/app/services/article-service.ts
+++ src/app/services/article-service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
+import {ArticleRepositoryService} from "../repository/article-repository-service";
+import {Observable} from "rxjs";
+import {Article} from "../entities/article.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
 
-  // TODO: Inject ArticleRepositoryService
-  constructor() { }
+  constructor(private articleRepo: ArticleRepositoryService) { }
 
   // TODO: Implement ArticleRepository methods
+  fetchAllArticlesAbout(keyword: string): Observable<Article[]> {
+    return this.articleRepo.fetchAllArticlesAbout(keyword);
+  }
 }
