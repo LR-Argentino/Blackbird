f750e4b4f5f783783036e14e9e6ce82b870f8707
luca <luca.argentino@outlook.com>
2022-04-20 15:38:31 +0200

    Add logic for Article Service

--- src/app/repository/article-repository-service.ts
+++ src/app/repository/article-repository-service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import {ArticleRepositoryInterface} from "../entities/articleRepository.interface";
 import {Article} from "../entities/article.interface";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -8,21 +11,21 @@ import {Article} from "../entities/article.interface";
 export class ArticleRepositoryService implements ArticleRepositoryInterface{
 
   // TODO: Inject HttpClient
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   // TODO: add fetchAllArticlesAbout logic
-  fetchAllArticlesAbout(keyword: string): Article[] {
-    return [];
+  fetchAllArticlesAbout(keyword: string): Observable<Article[]> {
+    return this.http.get<Article[]>(environment.newsApiUrl + 'q=' + keyword + "&pageSize=100&apiKey=" + environment.API_KEY);
   }
 
   // TODO: add fetchAllArticlesFromTo logic
-  fetchAllArticlesFromTo(from: string, to: string): Article[] {
-    return [];
+  fetchAllArticlesFromTo(searchByItem:string, keyword: string, from: string, to: string): Observable<Article[]>{
+    return this.http.get<Article[]>(environment.newsApiUrl + searchByItem + '=' + keyword + '&from' + '=' + from + '&to=' + to +"&apiKey=" + environment.API_KEY);
   }
 
   // TODO: add fetchAllArticlesPublishedBy logic
-  fetchAllArticlesPublishedBy(publisher: string): Article[] {
-    return [];
+  fetchAllArticlesPublishedBy(publisher: string): Observable<Article[]>  {
+    return this.http.get<Article[]>(environment.newsApiUrl + 'domains=' + publisher);
   }
 
 
