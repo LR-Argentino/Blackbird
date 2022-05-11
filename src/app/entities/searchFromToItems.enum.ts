19bd1e06ad00cb6bbdff55779870730058741010
luca <luca.argentino@outlook.com>
2022-05-11 11:29:02 +0200

    add url builder

---
+++ src/app/entities/searchFromToItems.enum.ts
@@ -0,0 +1,6 @@
+export enum SearchFromToItems {
+  keyword = 'q',
+  title = 'qInTitle',
+  sources = 'sources',
+  domains = 'domains',
+};
--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersChoiceService} from "../../services/users-choice.service";
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-
-  constructor() { }
+  private clickedTopic: object = {};
+  constructor(private userChoice: UsersChoiceService) { }
 
   ngOnInit() {
+  }
+
+  // TODO: add click logic (user shoudnt add twice the same topic)
+  getTopic(event: any) {
+    const topicValue = event.target.innerText;
+    if (this.clickedTopic.hasOwnProperty(topicValue)) {
+      event.target.style.backgroundColor = "";
+    }else {
+
+    }
+
+
+  }
+
+  listChoice() {
+    const choiceList = this.userChoice.listUsersChoice();
+    for (let i = 0; i < choiceList.length; i++) {
+      console.log(choiceList[i]);
+    }
   }
 
 }
